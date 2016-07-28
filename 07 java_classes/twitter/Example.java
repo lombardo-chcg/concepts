9589c6bddcefa844c0708e9e74d5b7d280c5b1ca
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-28 06:57:42 -0500

    Add Hashes, Maps

--- 07 java_classes/twitter/Example.java
+++ 07 java_classes/twitter/Example.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;  // collection of unique items
+import java.util.TreeSet; // ordered Set
 
 import com.lombardo.Tweet;
 import com.lombardo.Tweets;
@@ -12,31 +19,75 @@ public class Example {
       "@bob's in the #house!",
       new Date()
     );
-
+    
     Tweet tweet2 = new Tweet(
       "bob2",
       "hey @bob",
       new Date()
     );
-
+    
     System.out.printf("Tweet info: %s %n", tweet);
     System.out.println("Words in this tweet are:");
     for (String word : tweet.getWords()) {
       System.out.println(word);
     }
-
+    
     Tweet[] tweets = {tweet2, tweet};
     Arrays.sort(tweets);
     //for (Tweet t : tweets) {
-    //  System.out.println(t);
+    //  System.out.println(t); 
     //}
-
+    
     Tweets.save(tweets);
-
+    
     Tweet[] reloadedTweets = Tweets.load();
-
+    
     for (Tweet t : reloadedTweets) {
-      System.out.println(t);
+      System.out.println(t); 
     }
-  }
+    
+    for (String hashtag : tweet.getHashTags()) {
+      System.out.println("Hashtag present: " + hashtag); 
+    }
+    
+    Set<String> allHashTags = new HashSet<String>();
+    Set<String> allMentions = new TreeSet<String>();
+    
+    for (Tweet t : tweets) {
+      allHashTags.addAll(t.getHashTags());
+      allMentions.addAll(t.getMentions());
+    }
+    System.out.printf("Hash tags: %s %n", allHashTags);
+    System.out.printf("Mentions: %s %n", allMentions);
+    
+
+    // Hash Tag counts
+    Map<String, Integer> hashTagCount = new HashMap<String, Integer>();
+    for (Tweet t : tweets) {
+      for (String hashTag : t.getHashTags()) {
+        Integer count = hashTagCount.get(hashTag);
+        if (count == null) {
+          count = 0;
+        }
+        count ++;
+        hashTagCount.put(hashTag, count);
+      }
+    }
+   System.out.printf("Hash Tag counts: %s %n", hashTagCount);
+
+
+   // tweets by author
+   Map<String, List<Tweet>> tweetsByAuthor = new HashMap<String, List<Tweet>>();
+   for (Tweet t : tweets ) {
+    List<Tweet> authoredTweets = tweetsByAuthor.get(t.getAuthor());
+    if (authoredTweets == null) {
+      authoredTweets = new ArrayList<Tweet>();
+      tweetsByAuthor.put(t.getAuthor(), authoredTweets);
+    }
+    authoredTweets.add(t);
+   }
+   System.out.printf("tweets by author: %s %n", tweetsByAuthor);  
+    
+  }    
+  
 }
--- 07 java_classes/twitter/com/lombardo/Tweet.java
+++ 07 java_classes/twitter/com/lombardo/Tweet.java
@@ -1,52 +1,76 @@
 package com.lombardo;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class Tweet implements Comparable, Serializable {
+public class Tweet implements Comparable<Tweet>, Serializable {
   private static final long serialVersionUID = 4568266612889551649L; //stamps the class to allow changes after serialization without breaking the program
-
+  
   private String mAuthor;
   private String mDescription;
   private Date mCreationDate;
-
+  
   public Tweet(String author, String description, Date creationDate) {
     mAuthor = author;
     mDescription = description;
     mCreationDate = creationDate;
   }
-
+  
   @Override
   public String toString() {
     return String.format("Tweet: \"%s\" by %s on %s", mDescription, mAuthor, mCreationDate);
   }
-
+  
   @Override
-  public int compareTo(Object obj)  {
-    Tweet other = (Tweet) obj;
+  public int compareTo(Tweet other) {
     if (equals(other)) {
-      return 0;
+      return 0;  
     }
     int dateComp = mCreationDate.compareTo(other.mCreationDate);
     if (dateComp == 0) {
-      return mDescription.compareTo(other.mDescription);
+      return mDescription.compareTo(other.mDescription);  
     }
     return dateComp;
   }
-
+    
   public String getAuthor() {
-    return mAuthor;
+    return mAuthor; 
   }
-
+  
   public String getDescription() {
-    return mDescription;
+    return mDescription; 
   }
-
+  
   public Date getCreationDate() {
-    return mCreationDate;
+    return mCreationDate; 
   }
-
-  public String[] getWords() {
-    return mDescription.toLowerCase().split("[^\\w@#']+");
+  
+  public List<String> getWords() {
+    String[] words = mDescription.toLowerCase().split("[^\\w@#']+");
+    return Arrays.asList(words);
   }
+  
+  public List<String> getHashTags() {
+    return getWordsWithPrefix("#");
+  }
+  
+  public List<String> getMentions() {
+    return getWordsWithPrefix("@");
+  }
+  
+  private List<String> getWordsWithPrefix(String prefix) {
+    List<String> results = new ArrayList<String>();
+    for (String word : getWords()) {
+      if (word.startsWith(prefix)) {
+        results.add(word);  
+      }
+    }
+    return results;
+  }
+  
+  
+  
 }
